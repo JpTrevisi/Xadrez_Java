78d9aac411cf5d009ee3766809fcb6a4e0c46690
joaop <joaopedro.trevisi@gmail.com>
2023-07-05 09:08:25 -0300

    Implementacao da classe Position

---
+++ src/Application/Program.java
@@ -0,0 +1,11 @@
+package Application;
+
+import Boardgame.Position;
+
+public class Program {
+    public static void main(String[]arg){
+
+        Position position = new Position(3,5);
+        System.out.println(position);
+    }
+}

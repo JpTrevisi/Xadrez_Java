9948d3303cfdffef0142e5d5e17cf87aec6eacd0
joaop <joaopedro.trevisi@gmail.com>
2023-08-09 08:48:17 -0300

    Implementando movimento rainha

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -193,6 +193,7 @@ public class ChessMatch {
         placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('b', 1, new Knight(board, Color.WHITE));
         placeNewPiece('g', 1, new Knight(board, Color.WHITE));
+        placeNewPiece('d', 1, new Quenn(board, Color.WHITE));
 
         placeNewPiece('a',8,new Rook(board, Color.BLACK));
         placeNewPiece('e',8,new King(board, Color.BLACK));
@@ -209,5 +210,6 @@ public class ChessMatch {
         placeNewPiece('f',8,new Bishop(board, Color.BLACK));
         placeNewPiece('b',8,new Knight(board, Color.BLACK));
         placeNewPiece('g',8,new Knight(board, Color.BLACK));
+        placeNewPiece('d',8,new Quenn(board, Color.BLACK));
     }
 }
